ff22f44c73e1b0975c0f52b2e0d97ec0c5966389
Manuel Honoré de Sousa <sousa64manuel@gmail.com>
2022-11-05 19:24:02 +0100

    change section 01

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+// 
+const $toTop = document.getElementById("toTop");
+//
+window.onscroll = () => showScrollButtom();
+$toTop.onclick = () => scrollPageToTop();
+
+//Functions Bellow
+const showScrollButtom = () => {
+  const isAbleToShowButtom = document.body.scrollTop > 20 || document.documentElement.scrollTop > 20;
+  if (isAbleToShowButtom) return ($toTop.style.display = "flex");
+  return ($toTop.style.display = "none");
+};
+const scrollPageToTop = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
