21c4e02594f982242c6a85573cc508a9942edb9b
Manuel Honoré de Sousa <sousa64manuel@gmail.com>
2022-11-08 01:03:58 +0100

    working in menu - part 07

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,23 @@
 AOS.init();
 // 
 const $toTop = document.getElementById("toTop");
-//
+const $nav = document.querySelector("#nav")
+const $btnMenu = document.querySelector("#btn-mobile")
+
+//Activiti
 window.onscroll = () => showScrollButtom();
 $toTop.onclick = () => scrollPageToTop();
+$btnMenu.addEventListener("click", toggleMenu)
+$btnMenu.addEventListener("touchstart", toggleMenu)
+
 
 //Functions Bellow
+
+function toggleMenu(event) {
+  const isCliked = event.type === 'touchstart';
+  if (isCliked) event.preventDefault() 
+  $nav.classList.toggle("active")
+}
 const showScrollButtom = () => {
   const isAbleToShowButtom = document.body.scrollTop > 20 || document.documentElement.scrollTop > 20;
   if (isAbleToShowButtom) return ($toTop.style.display = "flex");
