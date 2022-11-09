f14529eeefbbbcaee27e864653d61416f454925c
Manuel Honoré de Sousa <sousa64manuel@gmail.com>
2022-11-09 01:13:11 +0100

    responsive done - v1.0 of site is done

--- src/js/index.js
+++ src/js/index.js
@@ -3,19 +3,24 @@ AOS.init();
 const $toTop = document.getElementById("toTop");
 const $nav = document.querySelector("#nav")
 const $btnMenu = document.querySelector("#btn-mobile")
+const $listItem = document.querySelectorAll(".list-item")
 
 //Activiti
 window.onscroll = () => showScrollButtom();
 $toTop.onclick = () => scrollPageToTop();
 $btnMenu.addEventListener("click", toggleMenu)
 $btnMenu.addEventListener("touchstart", toggleMenu)
+$listItem.forEach(item=> item.addEventListener("click", remuveActiveMobileMenu))
+$listItem.forEach(item=> item.addEventListener("touchstart", remuveActiveMobileMenu))
 
 
 //Functions Bellow
-
+function remuveActiveMobileMenu() {
+  $nav.classList.remove("active")
+}
 function toggleMenu(event) {
-  const isCliked = event.type === 'touchstart';
-  if (isCliked) event.preventDefault() 
+  const isTouch = event.type === 'touchstart';
+  if (isTouch) event.preventDefault() 
   $nav.classList.toggle("active")
 }
 const showScrollButtom = () => {
